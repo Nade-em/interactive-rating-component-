afc74b01855a0c7dd314a06745e283c16fdf19a2
enn-dee <nadeemahmad120808@gmail.com>
2022-11-23 19:54:02 +0530

    added components

---
+++ index.js
@@ -0,0 +1,14 @@
+let selectedRate = document.getElementById('ty-rating');
+document.getElementById('btn').addEventListener('click', (e) => {
+
+    window.location.href = "./indexTwo.html";
+    e.preventDefault();
+    if (document.getElementById === "one") {
+        document.getElementById("ty-rating").textContent = '1';
+    }
+
+
+});
+
+
+
